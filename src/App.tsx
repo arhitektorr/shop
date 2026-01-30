51168cc4edb16b723b317e7948e43982eb52f083
KOSUMOV LOMALI <groznyprogrammer@gmail.com>
2026-01-30 19:00:18 +0300

    Add Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import { Header } from './components/Header';
 
 function App() {
-  return <>Shop</>;
+  return (
+    <div className={'appContainer'}>
+      <Header />
+      <h1>Shop</h1>
+    </div>
+  );
 }
 
 export default App;
